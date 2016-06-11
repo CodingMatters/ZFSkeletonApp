3bfbe8bcaea3e0098322efabfb0e357342f9fe4a
gabbydgab <gamba@gabbydgab.com>
2016-06-11 12:18:37 +0800

    removed Application as submodule

--- config/application.config.php
+++ config/application.config.php
@@ -3,9 +3,6 @@
 return [
     // This should be an array of module namespaces used in the application.
     'modules' => [
-        'Site',
-        'Application',
-        'EdpModuleLayouts'
     ],
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => [
